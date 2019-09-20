23729cb62dbcc011f5bc9bfd4ba003da6fa5e15a
nearjay06 <nearjoan2018@gmail.com>
2019-09-20 17:44:06 +0300

    create git repo,git branch, public,src folders and package.json, index.html files

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import REACTDOM from "react-dom";
+import './style/style.css'
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+
+
+class App extends React.Component{
+    render(){
+        return (
+            <div> Hello, welcome to the Blog.</div>
+        )
+    }
+
+}
+    
+class Content extends React.Component{
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+            
+    //     }
+    // }
+    render(){
+        return (
+            
+        )
+    }
+
+}
+    
+
+
+
+
+
+
+
+export default Content;
+REACTDOM.render(<App />, document.querySelector("#book"));
