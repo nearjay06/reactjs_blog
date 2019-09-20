64a320861c7f25b6a7113bbef8637260a1cfec38
nearjay06 <nearjoan2018@gmail.com>
2019-09-20 17:51:31 +0300

    modify index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import REACTDOM from "react-dom";
 import './style/style.css'
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 
 class App extends React.Component{
